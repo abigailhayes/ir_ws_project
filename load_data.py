bbd113e90cd5cc12c4f1af6618ea756bb22229de
abigailhayes <abigail.joy.hayes@gmail.com>
2022-10-17 19:22:27 +0200

    Update to variable name

--- load_data.py
+++ load_data.py
@@ -9,7 +9,7 @@ os.chdir(os.path.dirname(sys.argv[0]))
 print(os.getcwd())
 
 # Read in 'candidates' data:
-hq_can = pandas.read_json(path_or_buf='candidates/livivo_hq_100_candidates.jsonl', lines=True)
-hq = pandas.read_json(path_or_buf='candidates/livivo_hq_1000.jsonl', lines=True)
+hq_can_train = pandas.read_json(path_or_buf='candidates/livivo_hq_100_candidates.jsonl', lines=True)
+hq_train = pandas.read_json(path_or_buf='candidates/livivo_hq_1000.jsonl', lines=True)
 hq_can_test = pandas.read_json(path_or_buf='candidates/livivo_hq_test_100_candidates.jsonl', lines=True)
 hq_test = pandas.read_json(path_or_buf='candidates/livivo_hq_test_100.jsonl', lines=True)
