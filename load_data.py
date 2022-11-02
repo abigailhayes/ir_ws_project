cd410046eb38d26df191bd8af7dd446c1230459b
abigailhayes <abigail.joy.hayes@gmail.com>
2022-11-02 22:40:37 +0100

    Test with all queries at once

--- load_data.py
+++ load_data.py
@@ -16,16 +16,14 @@ hq_can_test = pandas.read_json(path_or_buf='candidates/livivo_hq_test_100_candid
 hq_can_test.rename(columns={'qid:': 'qid'}, inplace=True)
 hq_test = pandas.read_json(path_or_buf='candidates/livivo_hq_test_100.jsonl', lines=True)
 
-# Testing extracting data - using query 1 from the training candidate dataset
-query_list = (hq_can_train[hq_can_train.qid.eq(1)])['candidates'][0]
-
 # Testing reading in the meta-data dataset as a substitute for the large data files
 meta_data = pandas.read_json(path_or_buf='meta-data.jsonl', lines = True)
 
-# Filter meta_data to the ids seen in query_list:
-data_list = meta_data[meta_data.DBRECORDID.isin(query_list)]
-
-
-
-
-
+# Obtaining all available abstracts relevant to each query
+query = pandas.DataFrame(columns = ['qid', 'qstr', 'recordid', 'title', 'abstract'])
+for i in range(len(hq_can_train)):
+    query_list = hq_can_train.candidates[i]
+    data_list = meta_data[meta_data.DBRECORDID.isin(query_list)]
+    extra_data = hq_can_train.loc[[i]]
+    new_data = data_list.merge(extra_data, how='cross')[['qid', 'qstr', 'DBRECORDID', 'TITLE', 'ABSTRACT']].rename(columns={'DBRECORDID': 'recordid', 'TITLE': 'title', 'ABSTRACT': 'abstract'})
+    query = pandas.concat([query, new_data])
