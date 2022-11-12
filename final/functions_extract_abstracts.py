f256a6b1e2d5b84f5dc8c6c6d385cee6d26c2a0f
abigailhayes <abigail.joy.hayes@gmail.com>
2022-11-12 13:48:32 +0100

    Created lists of file names

--- final/functions_extract_abstracts.py
+++ final/functions_extract_abstracts.py
@@ -1,1 +1,18 @@
+# Modules needed
 
+
+# File names
+name_queries = list('livivo_hq_test_100_candidates.jsonl')
+name_data_files = list('livivo_agris.jsonl',
+                       'livivo_medline_00.jsonl',
+                       'livivo_medline_01.jsonl',
+                       'livivo_medline_02.jsonl',
+                       'livivo_medline_03.jsonl',
+                       'livivo_medline_04.jsonl',
+                       'livivo_medline_05.jsonl',
+                       'livivo_medline_06.jsonl',
+                       'livivo_medline_07.jsonl',
+                       'livivo_medline_08.jsonl',
+                       'livivo_medline_09.jsonl',
+                       'livivo_medline_10.jsonl',
+                       'livivo_nlm.jsonl')
