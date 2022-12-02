069914823a10df1afea78e9acf00c5b605f91480
abigailhayes <abigail.joy.hayes@gmail.com>
2022-12-02 22:46:08 +0100

    Updated for the pooling csv

--- final/evaluation.py
+++ final/evaluation.py
@@ -1,17 +1,21 @@
+import pandas as pd
+
 # Read in pooled data relevance annotations. Will be a file with the document ID and a number for the relevance.
-pooled_data
+pooled_data = pd.read_csv("/Users/abigailhayes/Documents/IR/ir_ws_project/pooling_judgements.csv")
+pooled_data["Document"] = pooled_data['Document'].str.replace("\\'",'')
+pooled_data["Document"] = pooled_data['Document'].str.replace("\\‘",'')
 
 # Then want to loop through the ranked dictionary produced by one of the methods to add on the ranking.
 for key, value in ranking_dict:
-    if key in pooled_data["id"]:
+    if key in pooled_data["Document"]:
         pooled_data["ranking"] = value
         
 # Now to compute the Discounted Cumulative Gain
-pooled_data["dcg_work"] = pooled_data["relevance"]/log(pooled_data["ranking"]+1, base=2)
+pooled_data["dcg_work"] = pooled_data["Relevance"]/log(pooled_data["ranking"]+1, base=2)
 
 # The Ideal DCG is just based on the pooled data
-pooled_data["idcg_rank"] = pooled_data["relevance"].rank(ascending=False)
-pooled_data["idcg_work"] = pooled_data["relevance"]/log(pooled_data["idcg_rank"]+1, base=2)
+pooled_data["idcg_rank"] = pooled_data["Relevance"].rank(ascending=False)
+pooled_data["idcg_work"] = pooled_data["Relevance"]/log(pooled_data["idcg_rank"]+1, base=2)
 
 # Now can calculated the normalised DCG
 pooled_data["ndcg"] = pooled_data["dcg_work"]/pooled_data["idcg_work"]
